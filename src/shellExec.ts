7d5ef1c267b09283b56d881d536e2822ab82c98c
coderswat <coderswat@gmail.com>
2023-07-28 00:36:46 +0530

    added types and code formatted

--- src/shellExec.ts
+++ src/shellExec.ts
@@ -3,7 +3,10 @@ const shelljs = require("shelljs");
 const { exec } = shelljs;
 
 // function to execute commands in cli
-async function shellExec(command: string, silentMode: boolean = true): Promise<void> {
+async function shellExec(
+  command: string,
+  silentMode: boolean = true
+): Promise<void> {
   const result = exec(command, { silent: silentMode });
   if (result.code !== 0) {
     throw new Error(`Error executing command: ${command}`);
