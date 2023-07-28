061406c5c37a596c1b8a16fe4d1b6e36f5437a61
coderswat <coderswat@gmail.com>
2023-07-28 15:27:29 +0530

    updated helper.ts

--- src/helper.ts
+++ src/helper.ts
@@ -4,7 +4,6 @@ const path = require("path");
 function loadEnv(): void {
   // absolute dotenv path
   const dotenvPath: string = path.resolve(__dirname, "../config/", ".env");
-
   // require the dotenv file
   require("dotenv").config({ path: dotenvPath });
 }
